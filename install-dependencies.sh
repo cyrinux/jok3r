7b491190166afd03ac56bd224f209d14d2717831
Cyril Levis <levis.cyril@gmail.com>
2019-07-22 13:43:30 +0200

    install-dependencies less verbose

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -80,18 +80,6 @@ else
 fi
 print_delimiter
 
-
-# -----------------------------------------------------------------------------
-
-if ! [ -x "$(command -v ping)" ]; then
-    print_title "[~] Install network tools ..."
-    apt-get install -y iputils-ping traceroute whois
-else
-    print_title "[+] Network tools are already installed"
-fi
-print_delimiter
-
-
 # -----------------------------------------------------------------------------
 
 if ! [ -x "$(command -v tcpdump)" ]; then
@@ -117,7 +105,7 @@ print_delimiter
 
 print_title "[~] Install Python 2.7 + 3 and useful related packages (if missing)"
 apt-get install -y --ignore-missing python python2.7 python3 python-pip python3-pip 
-apt-get install -y --ignore-missing python-dev python3-dev python-setuptools python-virtualenv
+apt-get install -y --ignore-missing python-dev python3-dev python-setuptools 
 apt-get install -y --ignore-missing python3-setuptools python3-distutils
 apt-get install -y --ignore-missing python-ipy python-nmap python3-pymysql
 apt-get install -y --ignore-missing python3-psycopg2
@@ -206,16 +194,6 @@ if ! [ -x "$(command -v java)" ]; then
     apt-get install -y --ignore-missing default-jdk
 else
     print_title "[+] Java is already installed"
-fi
-print_delimiter
-
-# -----------------------------------------------------------------------------
-
-if ! [ -x "$(command -v nc)" ]; then
-    print_title "[~] Install netcat"
-    apt-get install -y --ignore-missing netcat
-else
-    print_title "[+] Netcat is already installed"
 fi
 print_delimiter
 
